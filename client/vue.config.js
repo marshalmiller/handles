0a7fa72e175226ba44cbcb5db7e6cb5aa4479351
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2022-04-24 19:34:11 -0400

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../server/public'),
+  
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/handles/'
+    : '/'
 
   devServer: {
     proxy: {
